383420fe8308028da572ae3aea3464cea0855305
Fagonar96 <fagonar96@gmail.com>
2019-06-14 19:06:17 -0600

    DigipetDML v2

--- DigiPetDML.sql
+++ DigiPetDML.sql
@@ -1,46 +1,52 @@
 INSERT INTO ROL (Nombre)
 VALUES
-	('Administrador'),
-	('Cliente'),
-	('Cuidador');
+	(1,'Administrador'),
+	(2,'Cliente'),
+	(3,'Cuidador');
 	
 INSERT INTO ESTADO (Nombre)
 VALUES
-	('Activado'),
-	('Desactivado'),
-	('Bloqueado');
+	(1,'Activado'),
+	(2,'Desactivado'),
+	(3,'Bloqueado');
 	
 INSERT INTO PROVINCIA (Nombre)
 VALUES
-	('San Jose'),
-	('Alajuela'),
-	('Cartago'),
-	('Heredia'),
-	('Guanacaste'),
-	('Puntarenas'),
-	('Limon');
+	(1,'San Jose'),
+	(2,'Alajuela'),
+	(3,'Cartago'),
+	(4,'Heredia'),
+	(5,'Guanacaste'),
+	(6,'Puntarenas'),
+	(7,'Limon');
 	
 INSERT INTO UNIVERSIDAD (Nombre)
 VALUES
-	('Instituto Tecnologico de Costa Rica'),
-	('Universidad de Costa Rica'),
-	('Universidad Nacional de Costa Rica');
+	(1,'Instituto Tecnologico de Costa Rica'),
+	(2,'Universidad de Costa Rica'),
+	(3,'Universidad Nacional de Costa Rica');
 
 INSERT INTO BADGE (Nombre)
 VALUES
-	('50 Caminatas'),
-	('100 Caminatas'),
-	('500 Caminatas'),
-	('Buen Servicio');
+	(1,'50 Caminatas'),
+	(2,'100 Caminatas'),
+	(3,'500 Caminatas'),
+	(4,'Buen Servicio');
 	
 INSERT INTO TAMANO (Nombre)
 VALUES
-	('S'),
-	('M'),
-	('L'),
-	('XL');
+	(1,'S'),
+	(2,'M'),
+	(3,'L'),
+	(4,'XL');
 	
 INSERT INTO SERVICIO (Nombre, Precio)
 VALUES
-	('Caminata', 30);
+	(1,'Caminata', 30);
 
+SELECT * FROM ROL;
+SELECT * FROM ESTADO;
+SELECT * FROM PROVINCIA;
+SELECT * FROM UNIVERSIDAD;
+SELECT * FROM BADGE;
+SELECT * FROM TAMANO;
