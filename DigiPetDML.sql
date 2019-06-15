63f3d7f7e6428afdf3aaefad80935b2b15dcb2f0
Fagonar96 <fagonar96@gmail.com>
2019-06-14 18:50:37 -0600

    DigipetDML v1

---
+++ DigiPetDML.sql
@@ -0,0 +1,46 @@
+INSERT INTO ROL (Nombre)
+VALUES
+	('Administrador'),
+	('Cliente'),
+	('Cuidador');
+	
+INSERT INTO ESTADO (Nombre)
+VALUES
+	('Activado'),
+	('Desactivado'),
+	('Bloqueado');
+	
+INSERT INTO PROVINCIA (Nombre)
+VALUES
+	('San Jose'),
+	('Alajuela'),
+	('Cartago'),
+	('Heredia'),
+	('Guanacaste'),
+	('Puntarenas'),
+	('Limon');
+	
+INSERT INTO UNIVERSIDAD (Nombre)
+VALUES
+	('Instituto Tecnologico de Costa Rica'),
+	('Universidad de Costa Rica'),
+	('Universidad Nacional de Costa Rica');
+
+INSERT INTO BADGE (Nombre)
+VALUES
+	('50 Caminatas'),
+	('100 Caminatas'),
+	('500 Caminatas'),
+	('Buen Servicio');
+	
+INSERT INTO TAMANO (Nombre)
+VALUES
+	('S'),
+	('M'),
+	('L'),
+	('XL');
+	
+INSERT INTO SERVICIO (Nombre, Precio)
+VALUES
+	('Caminata', 30);
+
